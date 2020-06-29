f8eb774c1913fa17c5f68d08f90d3ae7d3f88b39
Felix Peters <felix@albrechtpeters.de>
2020-06-29 10:47:55 +0200

    update frontend build

---
+++ build/precache-manifest.1f81121be080c46c69176333b6435447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6d67367c8e79773f12cfad3e206929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5beb62bd145455839c9f",
+    "url": "/static/css/main.5409fb29.chunk.css"
+  },
+  {
+    "revision": "1b84bd5753e2668e8ba1",
+    "url": "/static/js/2.a8a7ad0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8a7ad0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5beb62bd145455839c9f",
+    "url": "/static/js/main.25e35581.chunk.js"
+  },
+  {
+    "revision": "112737f28eda6650785d",
+    "url": "/static/js/runtime-main.5d6b4b2a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac62157b0d055a381d6c4ee7df5d6b36.js"
+  "/precache-manifest.1f81121be080c46c69176333b6435447.js"
 );
 
 self.addEventListener('message', (event) => {
