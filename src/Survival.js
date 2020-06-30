d202cf4bf1dcf87ed598c8757349f42e4ff9e1c5
Felix Peters <felix@albrechtpeters.de>
2020-06-30 16:41:56 +0200

    update layout of example page

--- src/Survival.js
+++ src/Survival.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Survival = () => {
   return (
-    <div class="flex flex-col bg-gray-300 px-4 py-3 mx-auto">
-      <h1 class="font-semibold text-2xl">Example: Survival Analysis</h1>
+    <div class="flex flex-col bg-gray-300">
+      <div class="bg-gray-100 shadow-md">
+        <h1 class="font-semibold text-2xl px-4 py-4">Example: Survival Analysis</h1>
+      </div>
       <SurvivalSummaryStatistics />
       <SurvivalData/>
     </div>
@@ -12,7 +14,7 @@ const Survival = () => {
 
 const SurvivalSummaryStatistics = () => {
   return (
-    <div class="bg-gray-100 p-2 mt-2 rounded-md shadow-md">
+    <div class="bg-gray-100 p-2 m-4 rounded-md shadow-md">
       <h2>Summary</h2>
     </div>
   );
@@ -20,7 +22,7 @@ const SurvivalSummaryStatistics = () => {
 
 const SurvivalData = () => {
   return (
-    <div class="bg-gray-100 p-2 mt-2 rounded-md shadow-md">
+    <div class="bg-gray-100 p-2 mx-4 mb-4 rounded-md shadow-md">
       <h2>Prognoses</h2>
     </div>
   );
