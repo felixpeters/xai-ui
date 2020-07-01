0e9033cd1ee97eb19fd39ba584f1e31936538cd9
Felix Peters <felix@albrechtpeters.de>
2020-07-01 16:21:00 +0200

    add chart library and first chart

--- src/SurvivalStatistics.js
+++ src/SurvivalStatistics.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Chart } from 'react-charts';
 
 const SurvivalStatistics = () => {
+  const data = React.useMemo(
+    () => [
+      {
+        label: 'Series 1',
+        data: [
+          [0, 1],
+          [1, 2],
+          [2, 4],
+          [3, 2],
+          [4, 7],
+        ],
+      },
+      {
+        label: 'Series 2',
+        data: [
+          [0, 3],
+          [1, 1],
+          [2, 5],
+          [3, 6],
+          [4, 4],
+        ],
+      },
+    ],
+    []
+  )
+
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'linear', position: 'bottom' },
+      { type: 'linear', position: 'left' },
+    ],
+    []
+  )
+
   return (
     <div class="flex-1 bg-gray-100 p-4 mr-2 rounded-md shadow-md">
       <h2 class="border-b border-gray-500 text-lg font-medium">Statistics</h2>
+      <div class="w-56 h-56 mt-2" >
+        <Chart data={data} axes={axes} />
+      </div>
     </div>
   );
 };
--- src/SurvivalSummary.js
+++ src/SurvivalSummary.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import SurvivalModel from './SurvivalModel';
 import SurvivalData from './SurvivalData';
+import SurvivalStatistics from './SurvivalStatistics';
 
 const SurvivalSummary = () => {
   return (
     <div class="flex flex-row m-4 items-stretch justify-center text-gray-700">
       <SurvivalModel />
       <SurvivalData />
-      <div class="flex-1 bg-gray-100 p-4 mr-2 rounded-md shadow-md">
-        <h2 class="border-b border-gray-500 text-lg font-medium">Statistics</h2>
-      </div>
+      <SurvivalStatistics />
     </div>
   );
 };
