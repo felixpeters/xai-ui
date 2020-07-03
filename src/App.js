29a90acfd08f8b4061c44bf60da99691928a767d
Felix Peters <felix@albrechtpeters.de>
2020-07-03 14:01:34 +0200

    add responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,15 +11,29 @@ import './App.css';
 
 const App = () => {
 
+  const [isOpen, toggleOpen] = useState(false);
+
   return (
       <Router>
-        <header class="bg-gray-900 flex justify-between items-center px-4 py-3">
-          <div class="text-white text-3xl font-medium">
-            <img src="./fpc_logo.png" class="h-20" alt=""/>
+        <header className="bg-gray-900 md:flex md:justify-between md:items-center md:px-4 md:py-3">
+          <div className="flex items-center justify-between px-4 py-3 md:p-0">
+            <div>
+              <img src="./fpc_logo.png" className="h-12 md:h-20" alt="Felix Peters Consulting"/>
+            </div>
+            <div className="md:hidden">
+              <button onClick={() => toggleOpen(!isOpen)} type="button" className="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
+                <svg className="h-8 w-8 fill-current" viewBox="0 0 24 24">
+                  {isOpen
+                    ? <path path fillRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/> 
+                    : <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+                  }
+                </svg>
+              </button>
+            </div>
           </div>
-          <nav className="flex">
-            <Link to="/" className="text-lg text-white rounded px-2 py-1 hover:bg-gray-800">Home</Link>
-            <Link to="/survival" className="text-lg text-white rounded px-2 py-1 hover:bg-gray-800">Survival Analysis</Link>
+          <nav className={`${isOpen ? 'block' : 'hidden'} px-2 pb-4 pt-2 md:flex md:p-0`}>
+            <Link to="/" className="block text-lg text-white rounded px-2 py-1 hover:bg-gray-800">Home</Link>
+            <Link to="/survival" className="block mt-1 text-lg text-white rounded px-2 py-1 hover:bg-gray-800 md:mt-0">Survival Analysis</Link>
           </nav>
         </header>
         <Switch>
@@ -30,9 +44,9 @@ const App = () => {
             <Home />
           </Route>
         </Switch>
-        <footer class="bg-gray-900 flex flex-col md:flex-row md:justify-between items-center px-4 py-3">
-          <div class="text-white text-sm font-thin">Social icons</div>
-          <div class="text-white text-sm font-thin">&copy; 2020 by Felix Peters. All rights reserved.</div>
+        <footer className="bg-gray-900 flex flex-col md:flex-row md:justify-between items-center px-4 py-3">
+          <div className="text-white text-sm font-thin">Social icons</div>
+          <div className="text-white text-sm font-thin">&copy; 2020 by Felix Peters. All rights reserved.</div>
         </footer>
       </Router>
   );
